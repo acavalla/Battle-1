b652d5f084d824573482310604c3abcdce8c04c0
Reeshul <reeshul@hotmail.com>
2021-01-20 16:55:20 +0000

    added sign in and play helper method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Adding & displaying player names' do
   scenario 'Personalising new games' do
-    visit('/')
-    fill_in('name', with: 'Anna')
-    fill_in('name2', with: 'Reeshul')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Welcome, Anna and Reeshul!'
   end
 end
--- spec/features/hit_point_display_spec.rb
+++ spec/features/hit_point_display_spec.rb
@@ -1,9 +1,6 @@
 feature 'seeing hit points' do
   scenario 'during gameplay' do
-    visit('/')
-    fill_in('name', with: 'Anna')
-    fill_in('name2', with: 'Reeshul')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "Reeshul: 100 HP"
   end
 end
